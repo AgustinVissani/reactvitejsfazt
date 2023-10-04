20bdf0f9172eb9be2d8a02840b78ab99d37fa08a
AgustinVissani <vissani1997@gmail.com>
2023-10-04 19:48:46 +0200

    second commit with json, and map

---
+++ src/assets/tasks.js
@@ -0,0 +1,22 @@
+ export const tasks = [
+  {
+    id: 0,
+    title: "Mi cero tarea",
+    descripcion: "Mi cero tarea descripcion",
+  },
+  {
+    id: 1,
+    title: "Mi primer tarea",
+    descripcion: "Mi primer tarea descripcion",
+  },
+  {
+    id: 2,
+    title: "Mi segunda tarea",
+    descripcion: "Mi segunda tarea descripcion",
+  },
+  {
+    id: 3,
+    title: "Mi tercer tarea",
+    descripcion: "Mi tercer tarea descripcion",
+  },
+];
