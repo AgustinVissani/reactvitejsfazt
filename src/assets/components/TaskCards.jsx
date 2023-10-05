f107196e29c804acb256dfe2a142a3b0f9a1a1a9
AgustinVissani <vissani1997@gmail.com>
2023-10-05 19:50:32 +0200

    mlñk

--- src/assets/components/TaskCards.jsx
+++ src/assets/components/TaskCards.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function TaskCards({ task }) {
+function TaskCards({ task, deleteTask }) {
+
   return (
     <div>
       <h1>{task.title}</h1>
       <p>{task.descripcion}</p>
+      <button onClick={() => deleteTask(task.id)}>Eliminar</button>
     </div>
   );
 }
